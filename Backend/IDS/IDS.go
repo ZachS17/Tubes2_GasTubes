4881e46cf95c52ff6ee7b0f07d40d89ec4d3a3ce
ZachS17 <130656022+ZachS17@users.noreply.github.com>
2024-04-20 14:34:42 +0700

    add: IDS (masih blm bener)
    pemanggilan DLS pada IDS masih salah karena mencari solusi bahkan ketika ketemu

---
+++ Backend/IDS/IDS.go
@@ -0,0 +1,163 @@
+package IDS
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func GetWikipediaLinks(pageName string) ([]string, error) {
+	// URL of the Wikimedia API endpoint
+	apiUrl := "https://en.wikipedia.org/w/api.php"
+
+	// Parameters for the API request
+	params := map[string]string{
+		"action": "parse",
+		"page":   pageName,
+		"format": "json",
+		"prop":   "links",
+	}
+
+	// Construct the URL with query parameters
+	req, err := http.NewRequest("GET", apiUrl, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating HTTP request: %v", err)
+	}
+	q := req.URL.Query()
+	for key, value := range params {
+		q.Add(key, value)
+	}
+	req.URL.RawQuery = q.Encode()
+
+	// Make the API request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error making API request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Decode the JSON response
+	var data map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return nil, fmt.Errorf("error decoding JSON response: %v", err)
+	}
+
+	// Check if "parse" key exists in the response
+	parseData, ok := data["parse"].(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("parse data not found in JSON response")
+	}
+
+	// Extract links from the response
+	linksData, ok := parseData["links"].([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("links data not found in parse section of JSON response")
+	}
+
+	linkTitles := make([]string, len(linksData))
+	for i, link := range linksData {
+		title := link.(map[string]interface{})["*"].(string)
+		linkTitles[i] = title
+	}
+
+	return linkTitles, nil
+}
+
+func isInArray(slice []string, value string) bool {
+	for _, item := range slice {
+		if item == value {
+			return true
+		}
+	}
+	return false
+}
+
+// rekursif
+// terima parameter visitedArray, target
+// func dls(currentPageName string, desiredPageName string, visited []string, solution []string, currentDepth int, desiredDepth int) []string {
+// 	// // pembuatan array visited dan solution
+// 	// if visited == nil {
+// 	// visited = make([]string, 0)
+// 	// solution = make([]string, 0)
+// 	// }
+// 	if currentDepth == desiredDepth { // sudah capai depth
+// 		fmt.Println("Link yang diperiksa:" + currentPageName)
+// 		fmt.Println("Yang dibandingkan: " + desiredPageName)
+// 		return solution
+// 	} else {
+// 		if currentPageName == desiredPageName { // ketemu
+// 			return solution
+// 		} else { // blm ketemu
+// 			if !isInArray(visited, currentPageName) { // blm dikunjungi
+// 				links, err := GetWikipediaLinks(currentPageName)
+// 				if err != nil {
+// 					return nil
+// 				} else {
+// 					for _, link := range links {
+// 						visited = append(visited, link)
+// 						solution = append(solution, link)
+// 						solution = dls(link, desiredPageName, visited, solution, currentDepth+1, desiredDepth)
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	// Add a return statement at the end of the function
+// 	return solution
+// }
+
+func dls(currentPageName string, desiredPageName string, visited []string, solution []string, currentDepth int, desiredDepth int) []string {
+	// fmt.Println("Link yang ditelusuri: " + currentPageName)
+	// fmt.Println("Link yang mau dicari: " + desiredPageName)
+
+	// utamain kedalaman
+	if currentDepth == desiredDepth {
+		// fmt.Println("Reached desired depth")
+		return solution
+	} else {
+		// utamain sudah visited
+		if isInArray(visited, currentPageName) {
+			// fmt.Println("Already visited:", currentPageName)
+			return solution
+		} else {
+
+			visited = append(visited, currentPageName)
+			links, err := GetWikipediaLinks(currentPageName)
+			// menangani error dapat link
+			if err != nil {
+				fmt.Println("Error getting links for", currentPageName, ":", err)
+				return solution
+			}
+			for _, link := range links {
+				// Recursively search for the desired page
+				newSolution := dls(link, desiredPageName, visited, solution, currentDepth+1, desiredDepth)
+				if len(newSolution) > len(solution) {
+					solution = newSolution
+				}
+				if len(newSolution) > 0 && newSolution[len(newSolution)-1] == desiredPageName {
+					return newSolution
+				}
+			}
+		}
+	}
+
+	return solution
+}
+
+func IDS(initialPageName string, desiredPageName string, desiredDepth int) ([]string, bool) {
+	visited := make([]string, 0)
+	solution := []string{initialPageName}
+	hasil := make([]string, 0)
+	var found bool = false
+	var currentDepth int = 0 // Start from depth 1
+	for !found && currentDepth <= desiredDepth {
+		hasil = dls(initialPageName, desiredPageName, visited, solution, currentDepth, desiredDepth) // Pass 0 as current depth to dls
+		if isInArray(hasil, desiredPageName) {
+			found = true
+		}
+		currentDepth = currentDepth + 1
+	}
+	return hasil, found
+}
--- Backend/main.go
+++ Backend/main.go
@@ -1,26 +1,32 @@
 package main
 
 import (
-	"Backend/BFS"
+	"Backend/IDS"
 	"fmt"
+	"time"
 )
 
 func main() {
-	var tujuan string = "Democratic Party"
-	solution := BFS.BFS("Barack Obama", tujuan, nil, nil)
-	var num int
-	num = 0
+	var awal string = "Barack Obama"
+	var tujuan string = "Michelle Obama"
+	mulai := time.Now()
+	solution, found := IDS.IDS(awal, tujuan, 1)
+	akhir := time.Now()
+	var num int = 0
+	num = 1
+	if !found { // tidak ketemu
+		fmt.Println("Tidak ditemui")
+	}
+	// visited, _ := IDS.GetWikipediaLinks(awal)
+	fmt.Println("Solusi: ")
 	for _, link := range solution {
-		fmt.Println(link)
+		fmt.Println(num, link)
 		num = num + 1
-		if link == tujuan {
-			break
-		}
 	}
 	fmt.Println("Banyak link yang ditelusuri:")
 	fmt.Println(len(solution))
 	fmt.Println("Banyak link untuk sampai:")
 	fmt.Println(num)
-	// fmt.Println(ketemu)
-	// fmt.Println(solution)
+	waktuEksekusi := akhir.Sub(mulai)
+	fmt.Println("Waktu eksekusi", waktuEksekusi)
 }
